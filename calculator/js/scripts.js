407be75b6cc1129fe2b215d9c04eb590987d1773
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-10-16 17:13:03 -0500

    add * / - co-authored by: Hunter Empey <LizardMak@gmail.com> co-authored-by: Daisy Orozco <daisyyy.ob@gmail.com>

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -1,10 +1,21 @@
+//business logic
 function add(number1, number2) {
     return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2){
+    return number1 * number2;
+}
+function divide(number1, number2){
+    return number1 / number2; 
 }
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-
+//user interface logic
 window.alert(add(number1, number2));
-//update
